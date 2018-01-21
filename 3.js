ff61a85960a4cf070edec9d46908d5cc3cd9ffab
lerastromtsova <v.stromtsova@yandex.ru>
2018-01-22 02:53:54 +0300

    First commit

---
+++ 3.js
@@ -0,0 +1,31 @@
+function startGame(player) {
+	dim1 = document.getElementById("dim1").value;
+	dim2 = document.getElementById("dim2").value;
+	alert(dim1);
+	alert(dim2);
+
+	row = document.getElementsByClassName('row')[0];
+	bigdiv = document.createElement("div");
+	bigdiv.setAttribute("class","col-md-12");
+	row.appendChild(bigdiv);
+
+	for (i=0;i<dim1;i++) {
+		div = document.createElement('div');
+        div.setAttribute("class","btn-group btn-group-lg");
+        div.setAttribute("role","group");
+        div.setAttribute("id",i);
+		for (j=0;j<dim2;j++) {
+			btn = document.createElement("button");
+			br = document.createElement("br");
+			btn.setAttribute("class","btn btn-default");
+			btn.setAttribute("id","cell"+i+j);
+			btn.innerHTML = player;
+			div.appendChild(btn);
+			div.appendChild(br);
+
+
+		}
+		bigdiv.appendChild(div);
+		row.removeChild(document.getElementById("init"));
+	}
+}
